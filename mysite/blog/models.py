d2c21438f8c5f3ebed376a279a1c9a469b3fc211
rs <serdiuk.r@gmail.com>
2020-09-10 22:53:13 +0300

    try to add tegs

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,27 +1,39 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+             self).get_queryset()\
+                          .filter(status='published')
 
 
 class Post(models.Model):
-    STATUS_CHOISCES = (
+    STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts')
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10,
-        choices=STATUS_CHOISCES,
-        default='draft'
-    )
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    objects = models.Manager()  # Менеджер по умолчанию
+    published = PublishedManager()  # Собственный менеджер
 
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -31,7 +43,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                        args=[self.publish.year,
-                                self.publish.strftime('%m'),
-                                self.publish.strftime('%d'),
-                                self.slug])
+               args=[self.publish.year,
+               self.publish.month,
+               self.publish.day,
+               self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,39 +5,18 @@ from django.views.generic import ListView
 
 
 class PostListView(ListView):
-    queryset = Post.objects.filter(status='published')
+    queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    # object_list = Post.published.all()
-    object_list = Post.objects.filter(status='published')
-
-    paginator = Paginator(object_list, 10)  # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                   'posts': posts})
-
-
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        slug=post,
-        status='published',
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=post,
+                 status='published',
+                 publish__year=year,
+                 publish__month=month,
+                 publish__day=day)
+    return render(request,
+          'blog/post/detail.html',
+          {'post': post})
